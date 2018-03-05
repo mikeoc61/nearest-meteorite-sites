de94835860c65c1fc24c7673dd0b6e71c9f3fc24
Michael OConnor <gmikeoc@gmail.com>
2018-03-04 22:04:56 -0600

    Initial commit

---
+++ meteor.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import math
+import requests
+from haversine import calc_dist as dist
+
+# Lat and long for 75063
+
+my_lat = 32.924702
+my_long = -96.959801
+my_loc = (my_lat, my_long)
+
+# If distance field is missing, populate field with very large number
+
+def get_dist(meteor):
+    return meteor.get('distance', math.inf)
+
+# Get Meteor impact coordinate data from NASA
+
+meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
+
+# Convert into JSON format
+
+meteor_data = meteor_resp.json()
+
+# Itterate through meteor data and add calculated distance from current location to data set
+# Skip if location coordinates are missing
+
+for meteor in meteor_data:
+    if not ('reclat' in meteor and 'reclong' in meteor): continue
+    meteor['distance'] = dist(float(meteor['reclat']), float(meteor['reclong']), my_loc[0], my_loc[1])
+
+# Sort meteor data by distance key made arbitrarly large if otherwise missing
+
+meteor_data.sort(key=get_dist)
+
+# Print out the 10 closest meteors to land near my location
+
+print ("Total meteors in dataset: {0:6d}".format(len(meteor_data)))
+
+for i in range(0,10):
+    print ("#{0:2d} closest meteor to me is: {1:6.2f} miles in {2:4s}".format(i+1, meteor_data[i]['distance'], meteor_data[i]['year']))
